316ae1a21d03ab63dbc1688f11fc8f77c6b9dd19
Chong Ji Xuan <65086213+CHONGJIXUAN@users.noreply.github.com>
2022-10-08 13:31:14 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Repair Appointment</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="img"></div>
+    <div class="center">
+        <h1>Login</h1>
+        <form action="index.php" method="post">
+            <div class="txt_field">
+                <input type="text" name="username" required>
+                <span></span>
+                <label>Username</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" required>
+                <span></span>
+                <label>Password</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" required>
+                <span></span>
+                <label>Confirm Password</label>
+            </div>
+            <div class="signup_link">
+                Have an account? <a href="register.php">Sign-in here</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
